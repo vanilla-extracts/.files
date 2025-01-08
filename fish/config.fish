fcd3093e8f423f339833a86c15aa70137ccb3b33
Charlotte Thomas <me@charlotte-thomas.me>
2025-01-08 19:35:46 +0100

    update .files

--- fish/config.fish
+++ fish/config.fish
@@ -15,13 +15,13 @@ setsid wal -i ~/Images/bg.png 2> /dev/null
 ssh-add /home/charlotte/.ssh/id_github_me
 set TYPST /home/charlotte/.config/typst_templates
 set PATH $PATH:/home/charlotte/.cargo/bin:/home/charlotte/.local/bin:/usr/sbin:/opt/node/bin:/opt/z3-4.12.2-x64-glibc-2.31/bin/
-set TYPST_FONT_PATH /usr/share/fonts:/home/charlotte/.fonts
+set -x TYPST_FONT_PATH /usr/share/fonts:/home/charlotte/.fonts
 set DOCSOFF /home/charlotte/Documents/docsoff
-set LD_LIBRARY_PATH $LD_LIBRARY_PATH:/home/charlotte/.lib
+set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH:/home/charlotte/.lib
 set DEV /home/charlotte/Documents/dev
-set EDITOR vim
-set VISUAL vim
-set CODE_EDITOR nvim
+set -x EDITOR vim
+set -x VISUAL vim
+set -x CODE_EDITOR nvim
 starship init fish | source
 alias ls="eza -l --icons=always --group-directories-first --hyperlink"
 alias listen="lsof -i -P -n | grep LISTEN"
@@ -34,4 +34,5 @@ end
 # pnpm end
 
 # opam configuration
+source /home/charlotte/credentials.fish
 source /home/charlotte/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
