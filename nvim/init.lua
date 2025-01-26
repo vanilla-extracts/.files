9b271a0e9df89dcb1f56b893aaa11e89e3c97590
Charlotte Thomas <me@charlotte-thomas.me>
2025-01-26 14:49:14 +0100

    change lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -187,8 +187,8 @@ require("lazy").setup(
                 options = {
                     icons_enabled = true,
                     theme = 'auto',
-                    component_separators = { left = '', right = '' },
-                    section_separators = { left = '', right = '' },
+                    component_separators = { left = '|', right = '|' },
+                    section_separators = { left = '', right = '' },
                     disabled_filetypes = {
                         statusline = {},
                         winbar = {},
