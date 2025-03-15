0dae9c7e20266f9929bd0d4875cd8fe9f3886583
Charlotte Thomas <me@charlotte-thomas.me>
2025-03-15 12:17:00 +0100

    niri: reset gaps
    scripts: add two scripts

---
+++ window_switcher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+niri msg -j windows \
+  | jq '.[] | "\(.id)\t\(.app_id): \(.title | tostring[:35])"' -r  \
+  | fuzzel -d -i -w 60 -p "lewindow? " \
+  | awk '{print $1}' \
+  | xargs niri msg action focus-window --id
