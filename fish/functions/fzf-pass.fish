1ac043e9be995e620590dacb6866a0a72b2a4b3e
Charlotte Thomas <me@charlotte-thomas.me>
2025-03-15 17:42:37 +0100

    fish: add fzf script

---
+++ fish/functions/fzf-pass.fish
@@ -0,0 +1,16 @@
+function fzf-pass
+    set PASSWORD_STORE_DIR $HOME/.password-store
+    set selected \
+        (begin
+            fd -tf . $PASSWORD_STORE_DIR | sed "s|^$PASSWORD_STORE_DIR/\(.*\)\.gpg\$|\1|"
+        end | fzf)
+
+    if [ -z "$selected" ]
+        echo Nothing selected
+        echo exiting...
+        return
+    end
+    
+    pass "$selected" -c
+
+end
