3d92e9ebe94443122289b330f471a477e6224030
Charlotte Thomas <me@charlotte-thomas.me>
2025-03-15 12:20:21 +0100

    niri: Mod+K for window switching

--- window_switcher.sh
+++ window_switcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 niri msg -j windows \
   | jq '.[] | "\(.id)\t\(.app_id): \(.title | tostring[:35])"' -r  \
-  | fuzzel -d -i -w 60 -p "lewindow? " \
+  | fuzzel -d -i -w 60 -p "Where? " \
   | awk '{print $1}' \
   | xargs niri msg action focus-window --id
