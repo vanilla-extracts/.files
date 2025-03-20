df8b3ef69e0e2453211ed828cfc443052523cf02
Charlotte Thomas <me@charlotte-thomas.me>
2025-03-20 15:43:35 +0100

    delete: show only current config

--- window_switcher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-niri msg -j windows \
-  | jq '.[] | "\(.id)\t\(.app_id): \(.title | tostring[:35])"' -r  \
-  | fuzzel -d -i -w 60 -p "Where? " \
-  | awk '{print $1}' \
-  | xargs niri msg action focus-window --id
