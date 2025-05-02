3fb18cf0e0309997e8a241827f6279acdc06f5c2
Charlotte Thomas <me@charlotte-thomas.me>
2025-05-02 22:51:04 +0200

    fish: alias nvim to hx

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,8 @@ function fish_greeting
 end
 
 for mode in default insert
-  bind --mode $mode \cf sessions 
-end 
+    bind --mode $mode \cf sessions
+end
 
 source $HOME/credentials.fish
 source $HOME/.config/fish/set.fish
@@ -15,4 +15,4 @@ source $HOME/.config/fish/jump.fish
 jump shell fish | source
 starship init fish | source
 
-ssh-add $HOME/.ssh/id_github_me 2> /dev/null
+ssh-add $HOME/.ssh/id_github_me 2>/dev/null
--- fish/fish_aliases.fish
+++ fish/fish_aliases.fish
@@ -4,3 +4,4 @@ alias listen 'lsof -i -P -n | grep LISTEN'
 alias ll 'ls -l --git-ignore'
 alias ls 'eza -l --icons=always --group-directories-first --hyperlink'
 alias lz lazygit
+alias nvim hx
