1f170617d8659d1bb846390119e8e9d6488253a8
Charlotte Thomas <me@charlotte-thomas.me>
2025-05-15 08:59:41 +0200

    fish: modify sponge delay to 2

--- fish/functions/api.fish
+++ fish/functions/api.fish
@@ -11,5 +11,5 @@ function api
         return
     end
     echo $inventory
-    ansible-playbook -i $inventory $argv
+    ansible-playbook --ask-become-pass -i $inventory $argv
 end
--- fish/set.fish
+++ fish/set.fish
@@ -3,4 +3,4 @@ set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
 set sponge_regex_patterns '^rm -rf \*$'
-set sponge_delay 0
+set sponge_delay 2
