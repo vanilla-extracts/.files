582d4bc87a8351d7ddc95947f4a83924a274a521
Charlotte Thomas <me@charlotte-thomas.me>
2025-05-15 14:45:03 +0200

    fish: fix

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,12 @@ end
 
 source $HOME/credentials.fish
 source $HOME/.cargo/env.fish
+source $HOME/.config/fish/set.fish
+source $HOME/.config/fish/env.fish
 source $HOME/.config/fish/fish_aliases.fish
 source $HOME/.config/fish/env.fish
 source $HOME/.config/fish/jump.fish
 source $HOME/.config/fish/pnpm.fish
-source $HOME/.config/fish/set.fish
 
 jump shell fish | source
 starship init fish | source
