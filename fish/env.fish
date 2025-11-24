c01f198b023117079d44bd12e4c07b0d0ae733b6
Charlotte Thomas <me@charlotte-thomas.me>
2025-11-24 16:20:01 +0100

    aerc: add explanation to :H

--- fish/env.fish
+++ fish/env.fish
@@ -2,7 +2,7 @@ set TYPST $HOME/.config/typst_templates
 set PATH $PATH:$HOME/.cargo/bin:$HOME/.local/bin:/usr/sbin:/opt/node/bin:/opt/z3-4.12.2-x64-glibc-2.31/bin/:$HOME/go/bin:/opt/teamspeak
 set -x TYPST_FONT_PATH /usr/share/fonts:$HOME/.fonts
 set DOCSOFF $HOME/Documents/docsoff
-set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH:$HOME/.lib
+set -x LD_LIBRARY_PATH
 set DEV $HOME/Documents/dev
 set -x CONFIG $HOME/.config
 set -x EDITOR vim
